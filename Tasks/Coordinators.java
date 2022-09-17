c3350ada0a3a2fcc5eeeef05f9045e7f746d4539
kadhar3425 <113160819+kadhar3425@users.noreply.github.com>
2022-09-17 12:40:33 +0530

    Add files via upload

---
+++ Tasks/Coordinators.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Coordinators{
+
+   public static void main(String[] args){
+     
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++)
+            arr[i]=s.nextInt();
+        Greater(arr,0,n-1);               
+ }
+  public static void Greater(int arr[],int start,int end){
+         int max=0;
+         int index=0;
+         while(start<=end){
+            if(max<arr[start]){
+              max=arr[start];
+              index=start;
+             }
+          start++;
+         }
+         System.out.print(max+" ");
+         max=0;
+         index++;
+         while(index<=end){
+           if(max<arr[index])
+             max=arr[index];
+        index++;
+        }
+        if(max!=0)
+          System.out.print(max);
+  }
+}
