af94591248ce6139febf576f8006f8770a7286bd
kadhar3425 <113160819+kadhar3425@users.noreply.github.com>
2022-09-26 14:10:21 +0530

    Update Brackets.java

--- Tasks/Brackets.java
+++ Tasks/Brackets.java
@@ -1,38 +1,38 @@
 import java.util.Scanner;
+
 public class Brackets{
-    public static void main(String[] args){
-         Scanner s=new Scanner(System.in);
-         String input=s.next();
-         int count=0;
-         for(int i=0;i<input.length()-1;i+=2){
-              char first=input.charAt(i);
-              char second=input.charAt(i+1);
-              char closing=Parenthese(first);
-              if(closing!=second){
-                  System.out.println("false");
-                  break;
-               }
-              else{
-                  count++;
-               }
-              System.out.println("true"+" "+first+second+"-"+count);
-              count=0;
-         }
-         
-   }
-   public static char Parenthese(char a){
-        switch(a){
-           case '(':
-                return ')';
-           case '{':
-                return  '}';
-           case  '[':
-                 return ']';
-           case  '<':
-                  return '>';
-           default:
-                  return ' ';
-           }
-   }
-   
+	 public static void main(String[] args){
+		 Scanner s=new Scanner(System.in);
+		 String a=s.next();
+		 char b[]=a.toCharArray();
+		 int c1=0,c2=0,c3=0;
+		 for(int i=0;i<b.length;i++){
+			 for(int j=i+1;j<=b.length-1;j++){
+				 if(b[i]=='{'&&b[j]=='}'){
+					 c1++;
+					 b[j]=' ';
+					 break;
+				 }
+				 else if(b[i]=='('&&b[j]==')'){
+					 c2++;
+					 b[j]=' ';
+					 break;
+				 }
+				 else if(b[i]=='['&&b[j]==']'){
+					c3++;
+					b[j]=' ';
+					break;
+				 }
+			 } 
+		}
+			         if(c1>0)
+					 System.out.println("{} "+"-"+c1);
+				     if(c2>0)
+				     System.out.println("()"+"-"+c2);
+					 if(c3>0)
+				     System.out.println("[] "+"-"+c3);
+		 
+	 }
 }
+				 
+				 
